58048d35544d18bbdbacfbc4879316bc6820f4ba
Jeanpierregaba <jeanpierregaba06@gmail.com>
2025-05-20 13:42:51 +0000

    update slider and landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -55,8 +55,8 @@ const LandingPage = () => {
   const heroImages = [
     "https://cdn.pixabay.com/photo/2013/02/10/14/49/operation-80124_1280.jpg",
     "https://media.istockphoto.com/id/1468497267/fr/photo/portrait-diversit%C3%A9-et-%C3%A9quipe-heureuse-de-m%C3%A9decins-avec-un-travail-d%C3%A9quipe-enthousiaste.jpg?s=2048x2048&w=is&k=20&c=wWww1A02KN_6SEgv3rwyXPjqi3V8ZRQGrc1KvRGwN1k=",
-    "https://www.freepik.com/premium-photo/senior-man-nurse-helping-with-walker-hospital-with-rehabilitation-assistance-physical-therapy-elderly-patient-caregiver-with-walking-support-clinic-mobility-wellness_311153640.htm#fromView=search&page=1&position=5&uuid=5b75ea3d-e8ec-4998-994a-265cb33a195e&query=personnel+soignant+avec+un+patient",
-    "https://www.freepik.com/free-photo/africa-humanitarian-aid-doctor-taking-care-patient_18955074.htm#fromView=search&page=1&position=6&uuid=5b75ea3d-e8ec-4998-994a-265cb33a195e&query=personnel+soignant+avec+un+patient"
+    "https://media.istockphoto.com/id/1400053534/fr/photo/jeune-patient-en-consultation-avec-son-m%C3%A9decin-un-m%C3%A9decin-afro-am%C3%A9ricain-montre-%C3%A0-un-patient.jpg?s=2048x2048&w=is&k=20&c=tu0P-VpZqmiBjrakm7qa3qzdEQpCL5Br42YCP_RnLQU=",
+    "https://media.istockphoto.com/id/1975788076/fr/photo/sourire-soins-de-sant%C3%A9-et-portrait-dinfirmi%C3%A8re-avec-un-patient-en-fauteuil-roulant-pour-une.jpg?s=2048x2048&w=is&k=20&c=v87-sqjhbF5pbxg0sVTPUJT8eWQbBTggm62ScIIV2fU="
   ];
 
   return (
